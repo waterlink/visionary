3cadee977fcb3f1091e2b2830796a94279b2841a
Alexey Fedorov <waterlink000@gmail.com>
2014-08-26 23:34:55 +0300

    Initial commit

---
+++ lib/visionary/version.rb
@@ -0,0 +1,3 @@
+module Visionary
+  VERSION = "0.0.1"
+end
---
+++ spec/visionary_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Visionary do
+  it 'has a version number' do
+    expect(Visionary::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
